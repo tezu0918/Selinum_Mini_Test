a52187725d02c51881c7bad07688a92652fd2819
tezu0918 <118744932+tezu0918@users.noreply.github.com>
2023-01-01 19:37:35 +0200

    Add files via upload

---
+++ locators.py
@@ -0,0 +1,10 @@
+
+
+store_acceseries = '//header/div[@id="ast-desktop-header"]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/nav[1]/div[1]/ul[1]/li[2]/a[1]'
+
+achor_braclet_path = '//body/div[@id="page"]/div[@id="content"]/div[1]/div[2]/main[1]/div[1]/ul[1]/li[1]/div[1]/a[1]/img[1]'
+Add_to_cart = "//button[contains(text(),'Add to cart')]"
+view_cartPath =  "//body/div[@id='page']/div[@id='content']/div[1]/div[1]/main[1]/div[1]/div[1]/div[1]/a[1]"
+productName ="//a[contains(text(),'Anchor Bracelet')]"
+coupon_feild = "//input[@id='coupon_code']"
+Apply_couponPath =  "//button[contains(text(),'Apply coupon')]"
